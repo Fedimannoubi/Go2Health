669bd4bc391065e1726904314e107f5dd77ba955
Kater Nada <Katernada2402@gmail.com>
2019-12-08 01:28:28 +0100

    Settings

--- app/src/main/java/com/example/bites/Settings.java
+++ app/src/main/java/com/example/bites/Settings.java
@@ -1,10 +1,19 @@
 package com.example.bites;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 
 public class Settings extends AppCompatActivity {
 
@@ -20,12 +29,46 @@ public class Settings extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
         }
+
     }
+
+    public void notification(){
+
+        SwitchCompat  toggle = findViewById(R.id.notificationSwitch);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // The toggle is enabled
+                    WaterNotification();
+                    Toast.makeText(Settings.this, "gggg", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void WaterNotification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,"5")
+                .setSmallIcon(R.drawable.notification_icon)
+                .setContentTitle("My notification")
+                .setContentText("Much longer text that cannot fit one line...")
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("Much longer text that cannot fit one line..."))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(1, builder.build());
+    }
+
+
 }
